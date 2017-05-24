d09b4361a280dfff0e443e0c4cecbf189f78e858
The Cap <xthecapx@gmail.com>
2017-05-24 10:00:13 -0500

    Added unit four grammar

---
+++ src/components/bookOne/unitFour/modules/Time.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="demostratives">
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <div class="panel-title">
+          <p class="no-margin"><kanji sound="じかん" kanji="時間"　en="Time"></kanji></p>
+        </div>
+      </div>
+      <div class="panel-body">
+        <div class="example-container">
+          <div class="example">
+            <kanji sound="いま" kanji="今"></kanji><kanji sound="なんじ" kanji="何時"></kanji><span class="end">ですか</span>。
+          </div>
+          <div class="example">
+            ...<kanji sound="ご" kanji="午"></kanji><kanji sound="ご" kanji="後"></kanji>12<kanji sound="じ" kanji="時"></kanji>15<kanji sound="ぷん" kanji="分"></kanji><span class="end">です</span>。
+          </div>
+          <div class="example">
+            <kanji sound="とうきょう" kanji="東京"></kanji><span class="particle">は</span><kanji sound="いま" kanji="今"></kanji><kanji sound="なんじ" kanji="何時"></kanji><span class="end">ですか</span>。
+          </div>
+          <div class="example">
+            ...<kanji sound="ご" kanji="午"></kanji><kanji sound="ぜん" kanji="前"></kanji>3<kanji sound="じ" kanji="時"></kanji>15<kanji sound="ぷん" kanji="分"></kanji><span class="end">です</span>。
+          </div>
+          <div class="example">
+            <span class="s1">ニューヨーク</span><span class="particle">は</span><kanji sound="いま" kanji="今"></kanji><kanji sound="なんじ" kanji="何時"></kanji><span class="end">ですか</span>。
+          </div>
+          <div class="example">
+            ...<kanji sound="ご" kanji="午"></kanji><kanji sound="ご" kanji="後"></kanji>2<kanji sound="じ" kanji="時"></kanji>15<kanji sound="ぷん" kanji="分"></kanji><span class="end">です</span>。
+          </div>
+          <div class="example">
+            <kanji sound="いま" kanji="今"></kanji><kanji sound="なんじ" kanji="何時"></kanji><span class="end">ですか</span>。
+          </div>
+          <div class="example">
+            ...<kanji sound="ご" kanji="午"></kanji><kanji sound="ご" kanji="後"></kanji>2<kanji sound="じ" kanji="時"></kanji><kanji sound="はん" kanji="半"></kanji><span class="end">です</span>。
+          </div>
+          <div class="example">
+            <kanji sound="あさ" kanji="朝"></kanji> 5<kanji sound="じ" kanji="時"></kanji><span class="end">です</span>。
+          </div>
+          <div class="example">
+            <kanji sound="あさ" kanji="朝"></kanji> 5<kanji sound="じ" kanji="時"></kanji><span class="end">です</span>。
+          </div>
+          <div class="example">
+            <kanji sound="ひる" kanji="昼"></kanji> 12<kanji sound="じ" kanji="時"></kanji><span class="end">です</span>。
+          </div>
+          <div class="example">
+            <kanji sound="よる" kanji="夜"></kanji> 8<kanji sound="じ" kanji="時"></kanji><span class="end">です</span>。
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import kanji from '../../../utils/Kanji'
+
+  export default {
+    components: {
+      kanji
+    }
+  }
+</script>
---
+++ src/components/bookOne/unitFour/modules/Vocabulary.js
@@ -0,0 +1,37 @@
+export const vocabulary = [
+  {
+    kanji: "午後",
+    sound: "ごご",
+    en: "p.m."
+  },
+  {
+    kanji: "午前",
+    sound: "ごぜん",
+    en: "a.m."
+  },
+  {
+    kanji: "東京",
+    sound: "とうきょう",
+    en: "Tokio"
+  },
+  {
+    kanji: "朝",
+    sound: "あさ",
+    en: "Morning"
+  },
+  {
+    kanji: "昼",
+    sound: "ひる",
+    en: "Noon"
+  },
+  {
+    kanji: "夜",
+    sound: "よる",
+    en: "Night"
+  },
+  {
+    kanji: "晩",
+    sound: "ばん",
+    en: "Night"
+  }
+];
